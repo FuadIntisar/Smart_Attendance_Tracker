40cb8d92c043b7413874a05cd7b3282a60f5bc0b
intisarfuad3@gmail.com <intisarfuad3@gamil.com>
2026-07-16 20:18:04 +0600

    Added logout functionality

--- lib/screens/student/student_home_screen.dart
+++ lib/screens/student/student_home_screen.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import '../login_screen.dart';
 
 class StudentHomeScreen extends StatelessWidget {
   const StudentHomeScreen({super.key});
+
+  void logout(BuildContext context) {
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const LoginScreen(),
+      ),
+      (route) => false,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,6 +20,12 @@ class StudentHomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Student Dashboard"),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () => logout(context),
+            icon: const Icon(Icons.logout),
+          ),
+        ],
       ),
 
       body: const Center(
--- lib/screens/teacher/teacher_home_screen.dart
+++ lib/screens/teacher/teacher_home_screen.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import '../login_screen.dart';
 
 class TeacherHomeScreen extends StatelessWidget {
   const TeacherHomeScreen({super.key});
+
+  void logout(BuildContext context) {
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const LoginScreen(),
+      ),
+      (route) => false,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,6 +20,12 @@ class TeacherHomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Teacher Dashboard"),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () => logout(context),
+            icon: const Icon(Icons.logout),
+          ),
+        ],
       ),
 
       body: const Center(
