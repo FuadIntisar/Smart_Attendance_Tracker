4e2ab07abe3070d95acf692e8d98ceaae4ee8e1f
intisarfuad3@gmail.com <intisarfuad3@gamil.com>
2026-07-17 11:18:04 +0600

    Improved student dashboard UI

--- lib/screens/student/student_home_screen.dart
+++ lib/screens/student/student_home_screen.dart
@@ -7,9 +7,7 @@ class StudentHomeScreen extends StatelessWidget {
   void logout(BuildContext context) {
     Navigator.pushAndRemoveUntil(
       context,
-      MaterialPageRoute(
-        builder: (context) => const LoginScreen(),
-      ),
+      MaterialPageRoute(builder: (context) => const LoginScreen()),
       (route) => false,
     );
   }
@@ -28,13 +26,71 @@ class StudentHomeScreen extends StatelessWidget {
         ],
       ),
 
-      body: const Center(
-        child: Text(
-          "Welcome Student",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Card(
+              elevation: 3,
+              child: ListTile(
+                leading: const CircleAvatar(
+                  radius: 28,
+                  child: Icon(Icons.person),
+                ),
+                title: const Text(
+                  "Student",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                subtitle: const Text("Welcome Back"),
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.qr_code_scanner),
+                label: const Text(
+                  "Give Attendance",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 15),
+
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.history),
+                label: const Text(
+                  "Attendance History",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 15),
+
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.person_outline),
+                label: const Text(
+                  "Profile", 
+                style: TextStyle(fontSize: 18)
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
